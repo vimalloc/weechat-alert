bc71189d3122e085d085f036cc02c913b8e5d45e
Landon GB <landogbland@gmail.com>
2016-05-12 09:41:55 -0600

    More work on hdata extraction

--- src/relay.rs
+++ src/relay.rs
@@ -35,26 +35,28 @@ mod weechat {
     }
 
     struct HData {
-        paths: Vec<Pointer>,
+        paths: Vec<PPath>,
         keys: HashMap<String, DataType>
     }
 
-    struct Pointer {
+    struct PPath {
         path: String,
         pointer: String,
     }
 
     enum DataType {
+        Buf(String),        // TODO make this a vector of u8 bytes instead
         Chr(char),
         Int(i32),
-        Ptr(Pointer),
-        Str(Option<String>),  // Option encodes the idea of a null string
-        Arr(Vec<DataType>),  // TODO
+        Lon(i64),
+        Ptr(Option<String>),
+        Str(Option<String>),
+        Tim(i32),
+        Arr(Vec<DataType>),
     }
 
-    struct ExtractString {
-        null: bool,
-        str: String,
+    struct ExtractedData {
+        value: DataType,
         bytes_read: usize,
     }
 
@@ -93,7 +95,7 @@ mod weechat {
 
     /// Converts a 4 byte array slice into a 32 bit signed integer. The bytes
     /// are assumed to be encoded in a big-endian format
-    fn bytes_to_int(byte_array: &[u8]) -> i32 {
+    fn bytes_to_i32(byte_array: &[u8]) -> i32 {
         assert!(byte_array.len() == 4, "Array isn't 4 bytes, cannot cast to int");
 
         // Re-arrange bytes from big to little-endian (so we can transmute them)
@@ -109,6 +111,11 @@ mod weechat {
         }
     }
 
+    // TODO for all these methods, maybe it would be clearer or make more sense
+    //      if we passed the start position (and end pos?) in as args, instead of
+    //      sending in smaller array slices to these functions? I don't know right
+    //      off hand, but it's somethign to think on
+
     /// Given a byte array which contains an encoded str, pull the string out
     /// and return it. The protocol from strings are:
     ///
@@ -117,35 +124,76 @@ mod weechat {
     ///
     /// Note: An empty string is valid, in this cass length will be 0. A NULL
     ///       string is also valid, it has length of -1. This is encoded in the
-    ///       ExtractString struct
-    fn extract_string(data: &[u8]) -> ExtractString {
+    ///       ExtractedData struct
+    fn extract_string(data: &[u8]) -> ExtractedData {
         // Sanity checks
         assert!(data.len() >= 4, "Not enough bytes in array to extract string");
 
         // Get the start and end limits for this string
         let mut start = 0;
         let mut end = 4;
-        let str_size = bytes_to_int(&data[start..end]);
+        let str_size = bytes_to_i32(&data[start..end]);
         start = end;
         end += str_size as usize;
+        assert!(data.len() >= end, "Not enough bytes in array to extract string");
 
         // Pull out and return the string
         match str_size {
-            0   => ExtractString {  // Empty string
-                       null: false,
-                       str: String::from(""),
-                       bytes_read: end
-                   },
-            -1  => ExtractString {  // Null string
-                       null: true,
-                       str: String::from(""),
-                       bytes_read: end
-                   },
-            _   => ExtractString {  // Null string
-                      null: false,
-                      str: String::from(from_utf8(&data[start..end]).unwrap()),
+            0  => ExtractedData {  // Empty string
+                      value: DataType::Str(Some(String::from(""))),
                       bytes_read: end
-                   },
+                  },
+            -1 => ExtractedData {  // Null string
+                      value: DataType::Str(None),
+                      bytes_read: end
+                  },
+            _  => ExtractedData {  // Normal string
+                     value: DataType::Str(Some(String::from(from_utf8(&data[start..end]).unwrap()))),
+                     bytes_read: end
+                  },
+        }
+    }
+
+    /// Given a byte array which contains an ecnoded pointer, pull the pointer
+    /// out and return it. The protocol for pointers are:
+    ///
+    /// byte 0: i8, size of pointer
+    /// bytes 1 - ?: pointer
+    ///
+    /// Note: A null poniter is valid. It will have size 1, and the pointer
+    ///       value of 0
+    fn extract_pointer(data: &[u8]) -> ExtractedData {
+        assert!(data.len() >= 2, "Not enough bytes in array to extract pointer");
+        let ptr_size = data[0] as i8;
+        let start = 1;
+        let end = start + ptr_size as usize;
+        assert!(data.len() >= end, "Not enough bytes in array to extract string");
+
+        // Pull out pointer, check if it's null
+        let ptr = String::from(from_utf8(&data[start..end]).unwrap());
+        let value;
+        if ptr.len() == 1 && ptr == "0" {
+            value = DataType::Ptr(None);
+        } else {
+            value = DataType::Ptr(Some(ptr));
+        }
+
+        ExtractedData {
+            value: value,
+            bytes_read: end,
+        }
+    }
+
+    /// Given a byte array which contains an encodec char, pull the char out.
+    ///
+    /// Returns a tuple, where the first element is the character read and the
+    /// second element is how much data was read from the byte array
+    fn extract_char(data: &[u8]) -> ExtractedData {
+        assert!(data.len() >= 1, "Not enough bytes in array to extract char");
+        ExtractedData {
+            value: DataType::Chr(data[0] as char),
+            bytes_read: 1,
+
         }
     }
 
@@ -261,7 +309,7 @@ mod weechat {
         pub fn new(data: &[u8]) -> MessageHeader {
             // Headers has length of full message, we need to chop off the
             // legth of the header as we have already read that from the socket
-            let total_msg_length = bytes_to_int(&data[0..4]);
+            let total_msg_length = bytes_to_i32(&data[0..4]);
             let length = total_msg_length as usize - HEADER_LENGTH;
 
             // Pull compression out of bytes, and verify it's 1 or 0
@@ -283,7 +331,7 @@ mod weechat {
 
         pub fn new(data: &[u8]) -> Result<MessageData, WeechatError> {
             // First 4 bytes are the integer length of the command name
-            let identifier_length = bytes_to_int(&data[0..4]);
+            let identifier_length = bytes_to_i32(&data[0..4]);
             let start_pos = 4;
             let end_pos = 4 + identifier_length as usize;
             let identifier = from_utf8(&data[start_pos..end_pos]).unwrap();
@@ -308,8 +356,11 @@ mod weechat {
         fn parse_pong(data: &[u8]) -> MessageType {
             let obj_type = from_utf8(&data[0..3]).unwrap();
             assert!(obj_type == "str");
-            let result = extract_string(&data[3..data.len()]);
-            MessageType::StrData(result.str)
+            let extracted = extract_string(&data[3..data.len()]);
+            match extracted.value {
+                DataType::Str(Some(s)) => MessageType::StrData(s),
+                _                      => panic!("pong should be DataType::Str"),
+            }
         }
 
         fn parse_buffer_line_added(data: &[u8]) -> MessageType {
@@ -336,32 +387,86 @@ mod weechat {
             // Parse out paths
             start = end;
             end = data.len();
-            let extracted = extract_string(&data[start..end]);
             let mut paths = Vec::new();
-            for path in extracted.str.split(',') {
-                paths.push(path);
+            let extracted = extract_string(&data[start..end]);
+            let path_str = match extracted.value {
+                DataType::Str(Some(ref s)) => s,
+                _                          => panic!("Paths should be non-null DataType::Str"),
+            };
+            for path in path_str.split(',') {
+                paths.push(String::from(path));
             }
 
             // Parse out key names and types
             start += extracted.bytes_read;
-            let extracted = extract_string(&data[start..end]);
+            end = data.len();
             let mut keys = Vec::new();
-            for key in extracted.str.split(',') {
-                keys.push(key);
+            let extracted = extract_string(&data[start..end]);
+            let key_str = match extracted.value {
+                DataType::Str(Some(s)) => s,
+                _                      => panic!("Keys should be non-null DataType::Str"),
+            };
+            for key in key_str.split(',') {
+                keys.push(String::from(key));
             }
 
+            // Number of items in this hdata
             start += extracted.bytes_read;
+            end = start + 4;
+            let num_hdata_items = bytes_to_i32(&data[start..end]);
+            println!("{}", num_hdata_items);
+
+            // Pull out path pointers
+            start = end;
+            end = data.len();
+            let mut ppaths = Vec::new();
+            for path in paths {
+                let extracted = extract_pointer(&data[start..end]);
+                let pointer_str = match extracted.value {
+                    DataType::Ptr(Some(p)) => p,
+                    _                      => panic!("Pointer should be DataType::Ptr"),
+                };
+                let ppath = PPath{
+                    path: String::from(path),
+                    pointer: pointer_str,
+                };
+                ppaths.push(ppath);
+                start += extracted.bytes_read;
+            }
+
+            // Finally, we pull out the data for all of the keys that we have
+            let mut key_value_map = HashMap::new();
+            for key in keys {
+                let key_parse: Vec<&str> = key.split(':').collect();
+                let key_name = key_parse[0];
+                let key_type = key_parse[1];
+
+                let value = match key_type {
+                    "chr" => DataType::Chr('a'),
+                    "int" => DataType::Int(1),
+                    "lon" => DataType::Lon(1),
+                    "str" => DataType::Str(Some(String::from("foobar"))),
+                    "buf" => DataType::Buf(String::from("foobar")),
+                    "ptr" => DataType::Ptr(Some(String::from("1a2b3d4d5"))),
+                    "tim" => DataType::Tim(1321993456),
+                    "arr" => DataType::Arr(Vec::new()),
+                    _     => panic!("Received invalid key type"),
+                };
+                key_value_map.insert(String::from(key_name), value);
+            }
 
             // Debug, see what the rest of the data looks like
+            /*
             println!("Start byets:");
             for byte in &data[start..end] {
                 print!("{} ", byte);
             }
             println!("\nByets finished!\n\n");
+            */
 
             HData {
-                paths: Vec::new(),
-                keys: HashMap::new(),
+                paths: ppaths,
+                keys: key_value_map,
             }
         }
     }
