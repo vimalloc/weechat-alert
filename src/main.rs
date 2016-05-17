34dfdc07baf03778ca0a50a5afe2e0c643829616
Landon GB <landogbland@gmail.com>
2016-05-17 17:46:07 -0600

    Cleanup config parsing

--- src/main.rs
+++ src/main.rs
@@ -18,96 +18,64 @@ mod relay;
 
 use relay::Relay;
 
+
+struct Config {
+    host: String,
+    port: i32,
+    password: String,
+}
+
+fn parse_config() -> Result<Config, String> {
+    // Get config filepath
+    let homedir = try!(env::home_dir().ok_or("Cannot find home directory"));
+    let mut path = PathBuf::from(homedir);
+    path.push(".relay");
+    path.set_extension("toml");
+
+    // Open the file and read the data
+    let mut file = try!(File::open(&path).map_err(|e| format!("{}: {}", path.display(), e)));
+    let mut file_data = String::new();
+    try!(file.read_to_string(&mut file_data).map_err(|e| format!("{}: {}", path.display(), e)));
+
+    // Parse the config
+    let config: toml::Value = try!(file_data.parse().map_err(|errs| {
+        let mut err = "Error parsing config file:".to_string();
+        for e in errs {
+            err.push_str("\n  ");
+            err.push_str(Error::description(&e));
+        }
+        err
+    }));
+
+    // Get data and return
+    let host = try!(config.lookup("server").ok_or("'server' not found in the config file"));
+    let host = try!(host.as_str().map(|s| s.to_string()).ok_or("'server' is not a valid string"));
+
+    let pw = try!(config.lookup("password").ok_or("'password' not found in the config file"));
+    let pw = try!(pw.as_str().map(|s| s.to_string()).ok_or("'password' is not a valid string"));
+
+    let port = try!(config.lookup("port").ok_or("'port' not found in the config file"));
+    let port = try!(port.as_integer().map(|s| s as i32).ok_or("'port' is not an integer"));
+
+    Ok(Config {
+        host: host,
+        port: port,
+        password: pw,
+    })
+}
+
 fn main() {
-    // Get the path of the config file
-    let homedir = match env::home_dir() {
-        Some(path) => path,
-        None       => {
-            println!("Error: Could not find home directory");
-            exit(1);
-        }
-    };
-    let mut config_path = PathBuf::from(homedir);
-    config_path.push(".relay");
-    config_path.set_extension("toml");
-
-    // Open the config file
-    let mut file = match File::open(&config_path) {
-        Ok(file) => file,
-        Err(why) => {
-            println!("Error opening {}: {}", config_path.display(),
-                                             Error::description(&why));
-            exit(1);
-        },
-    };
-
-    // Read the contents of the config file into memory
-    let mut config_str = String::new();
-    match file.read_to_string(&mut config_str) {
-        Ok(_)    => (),
-        Err(why) => {
-            println!("Couldn't read {}: {}", config_path.display(),
-                                             Error::description(&why));
-            exit(1);
-        }
-    }
-
-    // Parse the toml
-    let config: toml::Value = match config_str.parse() {
+    // Parse config
+    let config = match parse_config() {
         Ok(config) => config,
-        Err(errs)   => {
-            println!("Error Parsing config file:");
-            for err in &errs {
-                println!("  {}", Error::description(err));
-            }
-            exit(1);
-        }
-    };
-
-    let host = match config.lookup("relay.server") {
-        Some(host) => match host.as_str() {
-            Some(s) => s.to_string(),
-            None    => {
-                println!("Error: 'server' option is not a valid string");
-                exit(1);
-            }
-        },
-        None       => {
-            println!("Error: 'server' option not found under [relay] block");
-            exit(1);
-        }
-    };
-
-    let password = match config.lookup("relay.password") {
-        Some(pw) => match pw.as_str() {
-            Some(s) => s.to_string(),
-            None    => {
-                println!("Error: 'password' option is not a valid string");
-                exit(1);
-            }
-        },
-        None       => {
-            println!("Error: 'password' option not found under [relay] block");
-            exit(1);
-        }
-    };
-
-    let port = match config.lookup("relay.port") {
-        Some(port) => match port.as_integer() {
-            Some(i) => i as i32,
-            None    => {
-                println!("Error: 'port' option is not an integer");
-                exit(1);
-            }
-        },
-        None       => {
-            println!("Error: 'port' option not found under [relay] block");
+        Err(e)     => {
+            println!("Error: {}", e);
             exit(1);
         }
     };
 
     // Run our program
-    let relay =  Relay::new(host, port, password);
+    let relay =  Relay::new(config.host, config.port, config.password);
     match relay.run() {
         Err(e) => println!("Error: {}", e),
         Ok(_) => ()
