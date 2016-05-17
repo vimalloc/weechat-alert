fa046438c9be4272ca1ede636cc8a5313b4c5df9
Landon GB <landogbland@gmail.com>
2016-05-17 10:44:15 -0600

    Parse without unwraps. This is ugly as hell

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::PathBuf;
+use std::process::exit;
 
 extern crate ears;
 extern crate toml;
@@ -18,47 +19,92 @@ mod relay;
 use relay::Relay;
 
 fn main() {
-    // Get and parse the config file
+    // Get the path of the config file
     let homedir = match env::home_dir() {
         Some(path) => path,
         None       => {
             println!("Error: Could not find home directory");
-            return;
+            exit(1);
         }
     };
     let mut config_path = PathBuf::from(homedir);
     config_path.push(".relay");
     config_path.set_extension("toml");
+
+    // Open the config file
     let mut file = match File::open(&config_path) {
         Ok(file) => file,
         Err(why) => {
             println!("Error opening {}: {}", config_path.display(),
                                              Error::description(&why));
-            return;
+            exit(1);
         },
     };
+
+    // Read the contents of the config file into memory
     let mut config_str = String::new();
     match file.read_to_string(&mut config_str) {
         Ok(_)    => (),
         Err(why) => {
             println!("Couldn't read {}: {}", config_path.display(),
                                              Error::description(&why));
-            return;
+            exit(1);
         }
     }
-    let config = match toml::Parser::new(config_str.as_ref()).parse() {
-        Some(config) => config,
-        None         => {
-            println!("Failed to parse config file. Verify it's valid toml");
-            return
+
+    // Parse the toml
+    let config: toml::Value = match config_str.parse() {
+        Ok(config) => config,
+        Err(errs)   => {
+            println!("Error Parsing config file:");
+            for err in &errs {
+                println!("  {}", Error::description(err));
+            }
+            exit(1);
         }
     };
 
-    // holy shit clean this up. Fucking terrible
-    let relay_config = config.get("relay").unwrap();
-    let host = relay_config.as_table().unwrap().get("server").unwrap().as_str().unwrap().to_string();
-    let port = relay_config.as_table().unwrap().get("port").unwrap().as_integer().unwrap() as i32;
-    let password = relay_config.as_table().unwrap().get("password").unwrap().as_str().unwrap().to_string();
+    let host = match config.lookup("relay.server") {
+        Some(host) => match host.as_str() {
+            Some(s) => s.to_string(),
+            None    => {
+                println!("Error: 'server' option is not a valid string");
+                exit(1);
+            }
+        },
+        None       => {
+            println!("Error: 'server' option not found under [relay] block");
+            exit(1);
+        }
+    };
+
+    let password = match config.lookup("relay.password") {
+        Some(pw) => match pw.as_str() {
+            Some(s) => s.to_string(),
+            None    => {
+                println!("Error: 'password' option is not a valid string");
+                exit(1);
+            }
+        },
+        None       => {
+            println!("Error: 'password' option not found under [relay] block");
+            exit(1);
+        }
+    };
+
+    let port = match config.lookup("relay.port") {
+        Some(port) => match port.as_integer() {
+            Some(i) => i as i32,
+            None    => {
+                println!("Error: 'port' option is not an integer");
+                exit(1);
+            }
+        },
+        None       => {
+            println!("Error: 'port' option not found under [relay] block");
+            exit(1);
+        }
+    };
 
     // Run our program
     let relay =  Relay::new(host, port, password);
